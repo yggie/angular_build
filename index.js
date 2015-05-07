05c5fff0cc88cde4378cb195a6afe99df4ffd7f7
chrisface <chrisface@gmail.com>
2015-05-08 00:02:54 +0100

    Add watch task for html and javascript combined with live reload

--- index.js
+++ index.js
@@ -5,18 +5,49 @@ var del = require('del');
 var mainBowerFiles = require('main-bower-files');
 var concat = require('gulp-concat');
 var bower = require('gulp-bower');
-
+var livereload = require('gulp-livereload');
 
 module.exports = function(gulp){
 
+  var config = {
+    files: {
+      js: {
+        app: ['src/**/*.js']
+      },
+      html: {
+        app: ['src/**/*.html']
+      }
+    }
+  };
+
   gulp.task('angular-build', [
-    'build-vendor', 'build-app-js', 'build-app-templates', 'build-app-less'
+    'build-vendor', 'build-app-js', 'build-app-html','build-app-templates', 'build-app-less'
     ], function(){
 
   });
 
-  gulp.task('build-app-js', function(){
+  gulp.task('watch', function() {
+    gulp.watch(config.files.js.app, ['build-app-js']);
+    gulp.watch(config.files.html.app, ['build-app-html']);
+    livereload.listen();
+  });
 
+  gulp.task('build-app-js', function(){
+    var stream = gulp.src(config.files.js.app)
+      .pipe(debug({title: 'The application javascript files'}))
+      .pipe(concat('application.js'))
+      .pipe(debug({title: 'The concated application javascript files'}))
+      .pipe(gulp.dest('./build/'))
+      .pipe(livereload());
+    return stream;
+  });
+
+  gulp.task('build-app-html', function(){
+    var stream = gulp.src(config.files.html.app)
+      .pipe(debug({title: 'The application html files'}))
+      .pipe(gulp.dest('./build/'))
+      .pipe(livereload());
+    return stream;
   });
 
   gulp.task('build-app-templates', function(){
@@ -29,19 +60,21 @@ module.exports = function(gulp){
 
   gulp.task('build-vendor', function() {
     var stream = gulp.src(mainBowerFiles())
-    .pipe(debug({title: 'The bower vendor files'}))
-    .pipe(concat('vendor.js'))
-    .pipe(debug({title: 'The concated vendor files'}))
-    .pipe(gulp.dest('./build/'));
+      .pipe(debug({title: 'The bower vendor files'}))
+      .pipe(concat('vendor.js'))
+      .pipe(debug({title: 'The concated vendor files'}))
+      .pipe(gulp.dest('./build/'))
+      .pipe(livereload());
     return stream;
   });
 
   gulp.task('build-vendor-dev', function() {
     var stream = gulp.src(mainBowerFiles({includeDev: true}))
-    .pipe(debug({title: 'The bower vendor files'}))
-    .pipe(concat('vendor.js'))
-    .pipe(debug({title: 'The concated vendor files'}))
-    .pipe(gulp.dest('./build/'));
+      .pipe(debug({title: 'The bower vendor files'}))
+      .pipe(concat('vendor.js'))
+      .pipe(debug({title: 'The concated vendor files'}))
+      .pipe(gulp.dest('./build/'))
+      .pipe(livereload());
     return stream;
   });
 
